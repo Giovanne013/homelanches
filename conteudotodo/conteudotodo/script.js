4c25d56b26d72b70f3e108c3a7f4d1de8bdfb2ca
Giovanne013 <133395139+Giovanne013@users.noreply.github.com>
2024-06-25 10:37:31 -0300

    paginaLanches
    paginalanches

---
+++ conteudotodo/conteudotodo/script.js
@@ -0,0 +1,17 @@
+$('.slick-carousel').slick({
+    //Primeiro slide
+    contente:false,
+    dots:true,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+});
+$('.one-time').slick({
+    autoplay: true,
+    autoplaySpeed: 2500,
+    infinite: true,
+    speed: 3000,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
